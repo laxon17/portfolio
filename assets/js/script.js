0f802d8dd95f445975c6855890f3d6ded6590bfd
laxon17 <lazarlalovic@gmail.com>
2021-02-08 19:24:09 +0100

    New models

---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+const backToTopButton = document.querySelector("#backToTop");
+
+window.addEventListener("scroll", showFunction);
+
+function showFunction() {
+  if (window.pageYOffset > 250) {   //Show the button
+    if(!backToTopButton.classList.contains("showBtn")) {
+      backToTopButton.classList.remove("hideBtn");
+      backToTopButton.classList.add("showBtn");
+      backToTopButton.style.display = "block";
+    }
+  } 
+  else {  //Hide the button
+    if(backToTopButton.classList.contains("showBtn")) {
+        backToTopButton.classList.remove("showBtn");
+        backToTopButton.classList.add("hideBtn");
+        setTimeout(function() {
+          backToTopButton.style.display = "none";
+        }, 250);
+    }
+  }
+}
+
+backToTopButton.addEventListener("click", scrollBack);
+
+function scrollBack () {
+  window.scrollTo(0, 0);
+}
+
+// Navigation button hide/show
+
+const downArrow = document.getElementsByClassName('downArrow')[0];
+const ul = document.getElementsByTagName('ul')[0]; 
+
+downArrow = addEventListener ("click", () => {
+  ul.classList.toggle("open");
+});
+
+
+
+
+
